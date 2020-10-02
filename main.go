8b86f16d9e68dbfe1e3f2c750eb0b913d8ca94ff
nospec <stephanemathieu@hotmail.com>
2020-10-02 20:02:17 +1000

    Create main.go

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+// create a struct that holds information to be displayed in our html file
+type Welcome struct {
+	Name string
+	Time string
+}
+
+// go app entrypoint
+func main() {
+	welcome := Welcome{"Annonymous", time.Now().Format(time.Stamp)}
+
+	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
+
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if name := r.FormValue("name"); name != "" {
+			welcome.Name = name
+		}
+		if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+	fmt.Println("Listening")
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
